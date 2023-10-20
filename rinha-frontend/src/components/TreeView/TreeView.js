cabb6ee9e8cc8ff2da5e3da703b4fc4ea64c8835
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-10-20 17:11:38 -0300

    TreeView, estrutura inicial

--- rinha-frontend/src/components/TreeView/TreeView.js
+++ rinha-frontend/src/components/TreeView/TreeView.js
@@ -1,8 +1,29 @@
-const TreeView = ({fileName, jsonData}) => {
+const TreeView = ({ fileName, jsonData }) => {
+
+  const Node = ({ level }) => {
+    console.log(level)
+    if (Array.isArray(level)) {
+      return level.map(child => <Node level={child} />)
+    } else {
+      // return (
+      //   Object.keys(level).map(k => {
+      //     return (
+      //       <>
+      //         <span style={{ color: 'red', fontSize: '20px' }}>{k}:</span>
+      //         { Array.isArray(level(k)) ? <Node key={level(k)} />: '[' } 
+      //       </>
+      //     )
+      //   })
+      // )
+    }
+  }
+
   return (
     <section className="treeview">
       <h2>{fileName}</h2>
-      { JSON.stringify(jsonData) }
+
+      <Node level={jsonData} />
+      {JSON.stringify(jsonData)}
     </section>
   )
 }
